9c78452f427f68628c9aac7467371b68ed248434
nairanee <aneeshnair.1988@gmail.com>
2024-07-28 15:54:56 +0000

    Script updated

--- python_scripts_learning/scripts/Math_Format_Strings.py
+++ python_scripts_learning/scripts/Math_Format_Strings.py
@@ -10,3 +10,8 @@ print (f"Division of two numbers: {(x / y):9.2f}")
 print (f"Mod of two numbers: {x%y}")
 print ("Mod of two numbers: %s" %(x%y))
 print ('Mod of two numbers {}:'.format(x%y))
+#num1 = int(input("Enter number1"))
+#num2 = int(input("Enter number2"))
+#result = num1%num2
+print ('Result of decimal precision {:100.2f}'.format(x/y))
+print ('Value of x: {0:>8} Value of y : {0:8<}'.format(x,y))
