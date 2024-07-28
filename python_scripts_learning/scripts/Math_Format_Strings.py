836bafe3d0d5800eba51f0e246d5ff04692f1d36
nairanee <aneeshnair.1988@gmail.com>
2024-07-28 15:06:48 +0000

    Script ofr Math operations & string formatting

---
+++ python_scripts_learning/scripts/Math_Format_Strings.py
@@ -0,0 +1,12 @@
+'''
+This script is to execute Math operations & string formatting
+
+'''
+x = 2
+y = 3
+
+print (f"Mutiplication of two numbers: {x*y}")
+print (f"Division of two numbers: {(x / y):9.2f}")
+print (f"Mod of two numbers: {x%y}")
+print ("Mod of two numbers: %s" %(x%y))
+print ('Mod of two numbers {}:'.format(x%y))
