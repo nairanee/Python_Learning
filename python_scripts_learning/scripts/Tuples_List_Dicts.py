a62e478375f10c18725f2e820156f887ec00603a
nairanee <aneeshnair.1988@gmail.com>
2024-07-29 08:55:45 +0000

    Script updated

--- python_scripts_learning/scripts/Tuples_List_Dicts.py
+++ python_scripts_learning/scripts/Tuples_List_Dicts.py
@@ -1,9 +1,21 @@
 '''
 This script is created to test Tuples, List and Dict
 '''
+#List sort, reverse , append, pop
 mylist = [10,20,30,40,50]
-reverse_list = mylist.reverse()
+
+mylist.sort()
 print (f'List of numbers skipping 1st value: {mylist[1:]}')
 print (f'List of numbers mid 2 value: {mylist[2:4]}')
-#print (f'reverse list: {revers_list}')
-type(reverse_list)
+#reverese list
+mylist.reverse()
+print (f'reverse list: {mylist}')
+#sort list
+mylist.sort()
+print('Sorted List : {}'.format(mylist))
+#append list
+mylist.append(92)
+print (f'Added 92 to list :{mylist}')
+#pop number ie delete from list
+mylist.pop()
+print ('Number popped is: %s' %(mylist))
